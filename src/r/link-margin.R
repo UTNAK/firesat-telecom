a762b3fb07702d04b735a0c3a49cfe68c4ffd2d5
James Steven Jenkins <sjenkins@studioj.us>
2025-02-24 19:53:39 -0500

    missing files from previous commits

--- src/r/link-margin.R
+++ src/r/link-margin.R
@@ -4,3 +4,26 @@ dBinv <- function(x) 10^(x / 10)
 spaceLoss <- function(spacePathLength, frequency) {
   2 * dB(3e8 / (4 * pi * spacePathLength * frequency))
 }
+
+informationRate <- function(bitRate) {
+  bitRate
+}
+
+erfcinv <- function (x) qnorm(x / 2, lower = FALSE) / sqrt(2)
+minEbOverN0 <- function(bitErrorRate) {
+  erfcinv(2 * bitErrorRate)^2
+}
+
+irradiance <- function(effectivePower, spaceLoss) {
+  effectivePower + spaceLoss
+}
+
+k_dB <- dB(1.380649e+23)
+combine_gt <- function(l) dB(Reduce('+', Map(dBinv, l)))
+ebOverN0 <- function(gOverT, irradiance, bitRate) {
+  irradiance + combine_gt(gOverT) + k_dB - dB(bitRate)
+}
+
+linkMargin <- function(ebOverN0, minEbOverN0) {
+  ebOverN0 - minEbOverN0
+}
